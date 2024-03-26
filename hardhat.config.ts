e33d07f6401bd69a79b5d97cc788d417495e67f7
moganesan <developer.moganesan@gmail.com>
2024-03-26 15:22:47 +0530

    hardhat-config updated

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,29 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
+import "@openzeppelin/hardhat-upgrades";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.24",
+  networks: {
+    polygon: {
+      url: "https://rpc-mumbai.maticvigil.com/",
+      accounts: [
+        "a398e538b2a032cac4f05357080b03acca3b1510e52c65db27f58a89228fb140",
+      ],
+    },
+  },
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.20",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    ],
+  },
 };
 
 export default config;
