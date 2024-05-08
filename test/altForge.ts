2934da26b36b42d99feadffc7ef5cf000b86b8d6
moganesan <developer.moganesan@gmail.com>
2024-05-08 23:42:38 +0530

    altForge test file added

--- test/altForge.ts
+++ test/altForge.ts
@@ -1,20 +1,10 @@
-import { expect } from "chai";
-import hre from "hardhat";
-import { time } from "@nomicfoundation/hardhat-toolbox/network-helpers";
+import AltForgeModule from "../ignition/AltForgeModule";
+import { ignition } from "hardhat";
 
-describe("Lock", function () {
-  it("Should set the right unlockTime", async function () {
-    const lockedAmount = 1_000_000_000;
-    const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-    const unlockTime = (await time.latest()) + ONE_YEAR_IN_SECS;
+describe("Alt Forge", function () {
+  it("Should deploy with empty vesting details", async function () {
+    const { altForge } = await ignition.deploy(AltForgeModule);
 
-    // deploy a lock contract where funds can be withdrawn
-    // one year in the future
-    const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-      value: lockedAmount,
-    });
-
-    // assert that the value is correct
-    expect(await lock.unlockTime()).to.equal(unlockTime);
+    console.log(altForge);
   });
 });
