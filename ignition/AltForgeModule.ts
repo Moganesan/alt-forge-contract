e330568ce849e8802aa3191b3016926edcadc209
Moganesan <developer.moganesan@gmail.com>
2024-05-13 23:56:33 +0530

    altForege module

--- ignition/AltForgeModule.ts
+++ ignition/AltForgeModule.ts
@@ -1,7 +1,11 @@
 import { buildModule } from "@nomicfoundation/ignition-core";
-
+import { ignition } from "hardhat";
 export default buildModule("AltForge", (m) => {
   const altForge = m.contract("AltForge");
+
+  const investToken = m.contract("USDT");
+  const rewardToken = m.contract("Reward");
+
   return {
     altForge,
   };
