b0fca3651ac04054af2c35765161917477fc7fec
moganesan <developer.moganesan@gmail.com>
2024-05-22 23:15:17 +0530

    check is leap year and getDaysInMonth functions added

--- contracts/AltForge.sol
+++ contracts/AltForge.sol
@@ -326,4 +326,49 @@ contract AltForge is
     function getTotalRaisePercentage() public view returns (uint256) {
         return (totalRaise * 100) / targetRaise;
     }
+
+    /**
+    @dev function for checking isLeap year
+     */
+    function isLeapYear(uint16 year) internal pure returns (bool) {
+        if (year % 4 != 0) {
+            return false;
+        } else if (year % 100 != 0) {
+            return true;
+        } else if (year % 400 != 0) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    /**
+    @dev function for getting month in days 
+    */
+    function getDaysInMonth(
+        uint8 month,
+        uint16 year
+    ) internal pure returns (uint8) {
+        if (
+            month == 1 ||
+            month == 3 ||
+            month == 5 ||
+            month == 7 ||
+            month == 8 ||
+            month == 10 ||
+            month == 12
+        ) {
+            return 31;
+        } else if (month == 4 || month == 6 || month == 9 || month == 11) {
+            return 30;
+        } else if (month == 2) {
+            if (isLeapYear(year)) {
+                return 29;
+            } else {
+                return 28;
+            }
+        } else {
+            revert("Invalid month");
+        }
+    }
 }
