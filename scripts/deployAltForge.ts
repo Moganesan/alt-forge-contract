b6228cf0172eb8418858a5d05d3e4e38130a0fcd
moganesan <developer.moganesan@gmail.com>
2024-06-19 03:15:29 +0530

    deploy script for deploying altForge contract added

---
+++ scripts/deployAltForge.ts
@@ -0,0 +1,20 @@
+import { formatEther, parseEther } from "viem";
+import hre from "hardhat";
+
+async function main() {
+  const wallet: any = await hre.viem.getWalletClient(
+    "0x17206eE0F5F452cc9EA68374e2fe7BC62400c3A1"
+  );
+  const altForge = await hre.viem.deployContract("AltForge", [], {
+    client: wallet,
+  });
+
+  console.log("AltForge Contract", altForge.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
