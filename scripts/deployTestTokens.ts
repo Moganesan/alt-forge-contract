cb62c05f09b5894a8b87bc6b235b026336f56d7e
moganesan <developer.moganesan@gmail.com>
2024-06-19 03:39:13 +0530

    script addded for deploying test tokens

---
+++ scripts/deployTestTokens.ts
@@ -0,0 +1,26 @@
+import { parseEther } from "viem";
+import hre from "hardhat";
+
+async function main() {
+  const wallet: any = await hre.viem.getWalletClient(
+    "0x17206eE0F5F452cc9EA68374e2fe7BC62400c3A1"
+  );
+  const USDT = await hre.viem.deployContract(
+    "Reward",
+    [parseEther("1000000")],
+    { client: wallet }
+  );
+  const REW = await hre.viem.deployContract("USDT", [parseEther("1000000")], {
+    client: wallet,
+  });
+
+  console.log("USDT Contract", USDT.address);
+  console.log("Reward Contract", REW.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
